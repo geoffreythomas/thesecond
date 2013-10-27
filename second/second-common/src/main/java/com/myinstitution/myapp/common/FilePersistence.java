9d678ded8e9a7f0f13a8b981447f38105f0c72ce
Geoffrey Thomas <geoffrey.thomas@gmail.com>
2013-10-28 02:05:53 +0530

    New module second-common

---
+++ second/second-common/src/main/java/com/myinstitution/myapp/common/FilePersistence.java
@@ -0,0 +1,53 @@
+package com.myinstitution.myapp.common;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.slf4j.Logger;
+
+import com.myinstitution.myapp.logger.LoggerFactory;
+
+public class FilePersistence {
+	private static Logger LOGGER = LoggerFactory
+			.getLogger(FilePersistence.class);
+
+	public static void write(Object object, String path) throws Exception {
+		try {
+			final ObjectOutputStream objectOutputStream = new ObjectOutputStream(
+					new FileOutputStream(new File(path)));
+			objectOutputStream.writeObject(object);
+			objectOutputStream.flush();
+			objectOutputStream.close();
+		} catch (FileNotFoundException e) {
+			LOGGER.error(MessageKeys.ERROR_WRITING_FILE + path);
+			throw new Exception(e);
+		} catch (IOException e) {
+			LOGGER.error(MessageKeys.ERROR_WRITING_FILE + path);
+			throw new Exception(e);
+		}
+	}
+
+	public static Object read(String path) throws Exception {
+		try {
+			final FileInputStream inputstream = new FileInputStream(new File(
+					path));
+			final ObjectInputStream objectInputStream = new ObjectInputStream(
+					inputstream);
+			return (Object) objectInputStream.readObject();
+		} catch (FileNotFoundException e) {
+			LOGGER.error(MessageKeys.ERROR_READING_FILE + path);
+			throw new Exception(e);
+		} catch (IOException e) {
+			LOGGER.error(MessageKeys.ERROR_READING_FILE + path);
+			throw new Exception(e);
+		} catch (ClassNotFoundException e) {
+			LOGGER.error(MessageKeys.ERROR_READING_FILE + path);
+			throw new Exception(e);
+		}
+	}
+}
---
+++ second/second-common/src/main/java/com/myinstitution/myapp/common/MessageKeys.java
@@ -0,0 +1,10 @@
+package com.myinstitution.myapp.common;
+
+public interface MessageKeys {
+
+    String ERROR_WRITING_FILE = "ERROR_WRITING_FILE:  ";
+    String ERROR_GENERIC = "ERROR_GENERIC {0}";
+    String ERROR_READING_FILE = "ERROR_READING_FILE {0}";
+    String ERROR_INCORRECT_ESAFE_CREDENTIALS = "ERROR_INCORRECT_ESAFE_CREDENTIALS";
+
+}
