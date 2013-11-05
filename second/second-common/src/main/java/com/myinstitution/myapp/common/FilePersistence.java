4b4f4e1768d779c67140f64fe680724944264114
Geoffrey Thomas <geoffrey.thomas@gmail.com>
2013-11-06 01:11:35 +0530

    Added new methods to FilePersistence.

--- second/second-common/src/main/java/com/myinstitution/myapp/common/FilePersistence.java
+++ second/second-common/src/main/java/com/myinstitution/myapp/common/FilePersistence.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 
 import com.myinstitution.myapp.logger.LoggerFactory;
@@ -46,6 +47,35 @@ public class FilePersistence {
 			LOGGER.error(MessageKeys.ERROR_READING_FILE + path);
 			throw new Exception(e);
 		} catch (ClassNotFoundException e) {
+			LOGGER.error(MessageKeys.ERROR_READING_FILE + path);
+			throw new Exception(e);
+		}
+	}
+
+	public static void writeFromByteArray(Object object, String path)
+			throws Exception {
+		try {
+			FileOutputStream fileOutputStream = new FileOutputStream(new File(
+					path));
+			IOUtils.write((byte[]) object, fileOutputStream);
+		} catch (FileNotFoundException e) {
+			LOGGER.error(MessageKeys.ERROR_WRITING_FILE + path);
+			throw new Exception(e);
+		} catch (IOException e) {
+			LOGGER.error(MessageKeys.ERROR_WRITING_FILE + path);
+			throw new Exception(e);
+		}
+	}
+
+	public static Object readToByteArray(String path) throws Exception {
+		try {
+			final FileInputStream inputstream = new FileInputStream(new File(
+					path));
+			return (Object) IOUtils.toByteArray(inputstream);
+		} catch (FileNotFoundException e) {
+			LOGGER.error(MessageKeys.ERROR_READING_FILE + path);
+			throw new Exception(e);
+		} catch (IOException e) {
 			LOGGER.error(MessageKeys.ERROR_READING_FILE + path);
 			throw new Exception(e);
 		}
